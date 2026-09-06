24a676231cad214e33c2b8a2399ed83437608c1d
Andrei C <andreic@Andreis-MacBook-Pro.local>
2026-09-06 19:13:10 -0400

    Refresh grocery checklist count immediately

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cut-coach-shell-v1';
+const CACHE='cut-coach-shell-v2';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('cut-coach-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
